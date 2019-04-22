40618e026bc19ca362c798ca8426057b42c7ca78
irenenene <irene.liu29@gmail.com>
2019-04-21 19:56:29 -0700

    Removed git diff

--- Tasks.py
+++ Tasks.py
@@ -1,10 +1,8 @@
 import User
 from User import User
-<<<<<<< HEAD:Tasks.py
 
-=======
+
 import datetime
->>>>>>> bbaf0d2c49cc77cd31e65ba2a7c2b02a1a309100:Tasks
 class Tasks:
     def __init__(self, taskName, taskStatus, startTime, endTime, timeSpent):
         self.taskName = taskName
@@ -13,11 +11,10 @@ class Tasks:
         self.endTime = endTime
         self.timeSpent = timeSpent
         self.user = User("Unassigned")
-<<<<<<< HEAD:Tasks.py
-		
+
     def getTask(self):
         return self.taskName + ' - ' + self.user.name + ' - ' + self.taskStatus
-=======
+
     def startWorkTime(self):
         self.startTime = datetime.datetime.now()
         return self.startTime
@@ -32,4 +29,3 @@ class Tasks:
         decMinWorked = minWorked/60
         self.timeSpent = self.timeSpent + hoursWorked + decMinWorked
         return format(self.timeSpent, '.2f')
->>>>>>> bbaf0d2c49cc77cd31e65ba2a7c2b02a1a309100:Tasks
